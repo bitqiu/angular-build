67158bbac7bdb25082516fb5d3f5b09c56baf605
bitqiu <i@bitqiu.cc>
2016-08-21 13:32:46 +0800

    first commit

---
+++ src/app/index.route.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      }).state('test', {
+        url: '/test',
+        templateUrl: 'app/test/test.html',
+        controller: 'TestController'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
